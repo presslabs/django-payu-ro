8eab4057450ce37cd457d41c6e898dca83ee78ce
Vlad Temian <vladtemian@gmail.com>
2016-12-05 11:17:12 +0200

    Make room for token example. Add a separate view/template called home.

--- example/example/demo/views.py
+++ example/example/demo/views.py
@@ -2,64 +2,81 @@ from payu.forms import PayULiveUpdateForm
 
 from django.shortcuts import render
 
-
-def home(request):
-    order = [
-        {
-            'PNAME': 'CD Player',
-            'PCODE': 'PROD_04891',
-            'PINFO': 'Extended Warranty - 5 Years',
-            'PRICE': '82.3',
-            'PRICE_TYPE': 'GROSS',
-            'QTY': '7',
-            'VAT':'20'
-        },
-        {
-            'PNAME': 'Mobile Phone',
-            'PCODE': 'PROD_07409',
-            'PINFO': 'Dual SIM',
-            'PRICE': '1945.75',
-            'PRICE_TYPE': 'GROSS',
-            'QTY': '3',
-            'VAT':'20'
-        },
-        {
-            'PNAME': 'Laptop',
-            'PCODE': 'PROD_04965',
-            'PINFO': '17" Display',
-            'PRICE': '5230',
-            'PRICE_TYPE': 'GROSS',
-            'QTY': '1',
-            'VAT':'20'
-        }
-    ]
-    details = {
-        'ORDER_REF': '789456123',
-        'ORDER_DATE': '2016-10-05 11:12:27',
-        'PRICES_CURRENCY': 'RON',
-        'ORDER_SHIPPING': '0',
-        'DISCOUNT': '55',
-        'PAY_METHOD': 'CCVISAMC',
-        'DESTINATION_CITY': 'Bucuresti',
-        'DESTINATION_STATE': 'Bucuresti',
-        'DESTINATION_COUNTRY': 'RO',
-        'TESTORDER': 'TRUE',
-        'BACK_REF': 'http://localhost:8000/',
-        'BILL_FNAME': 'Joe',
-        'BILL_LNAME': 'Doe',
-        'BILL_COUNTRYCODE': 'RO',
-        'BILL_PHONE': '+040000000000',
-        'BILL_EMAIL': 'joe.doe@gmail.com',
+ORDER = [
+    {
+        'PNAME': 'CD Player',
+        'PCODE': 'PROD_04891',
+        'PINFO': 'Extended Warranty - 5 Years',
+        'PRICE': '82.3',
+        'PRICE_TYPE': 'GROSS',
+        'QTY': '7',
+        'VAT':'20'
+    },
+    {
+        'PNAME': 'Mobile Phone',
+        'PCODE': 'PROD_07409',
+        'PINFO': 'Dual SIM',
+        'PRICE': '1945.75',
+        'PRICE_TYPE': 'GROSS',
+        'QTY': '3',
+        'VAT':'20'
+    },
+    {
+        'PNAME': 'Laptop',
+        'PCODE': 'PROD_04965',
+        'PINFO': '17" Display',
+        'PRICE': '5230',
+        'PRICE_TYPE': 'GROSS',
+        'QTY': '1',
+        'VAT':'20'
     }
+]
 
-    payu_dict = details.copy()
-    payu_dict['ORDER'] = order
+DETAILS = {
+    'ORDER_REF': '789456123',
+    'ORDER_DATE': '2016-10-05 11:12:27',
+    'PRICES_CURRENCY': 'RON',
+    'ORDER_SHIPPING': '0',
+    'DISCOUNT': '55',
+    'PAY_METHOD': 'CCVISAMC',
+    'DESTINATION_CITY': 'Bucuresti',
+    'DESTINATION_STATE': 'Bucuresti',
+    'DESTINATION_COUNTRY': 'RO',
+    'TESTORDER': 'TRUE',
+    'BACK_REF': 'http://localhost:8000/',
+    'BILL_FNAME': 'Joe',
+    'BILL_LNAME': 'Doe',
+    'BILL_COUNTRYCODE': 'RO',
+    'BILL_PHONE': '+040000000000',
+    'BILL_EMAIL': 'joe.doe@gmail.com',
+}
+
+
+
+def live_update(request):
+    payu_dict = DETAILS.copy()
+    payu_dict['ORDER'] = ORDER
 
     payu_form = PayULiveUpdateForm(initial=payu_dict)
 
-    return render(request, 'simple_payment.html', {
+    return render(request, 'live_update.html', {
         'form': payu_form,
-        'orders': order,
-        'details': details,
+        'orders': ORDER,
+        'details': DETAILS,
+        'order_hash': payu_form.fields['ORDER_HASH'].initial
+    })
+
+
+def tokens(request):
+    payu_dict = DETAILS.copy()
+    payu_dict['ORDER'] = ORDER
+    payu_dict['LU_TOKEN'] = '1'
+
+    payu_form = PayULiveUpdateForm(initial=payu_dict)
+
+    return render(request, 'tokens.html', {
+        'form': payu_form,
+        'orders': ORDER,
+        'details': DETAILS,
         'order_hash': payu_form.fields['ORDER_HASH'].initial
     })
--- example/example/urls.py
+++ example/example/urls.py
@@ -1,14 +1,20 @@
+from django.views.generic import TemplateView
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
+from example.demo import views as demo
+
+
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="home.html")),
     url(r'^admin/', include(admin.site.urls)),
+]
+
+urlpatterns += [
+    url(r'^live-update/$', view=demo.live_update, name='live_update'),
+    url(r'^tokens/$', view=demo.tokens, name='tokens'),
 ]
 
 urlpatterns += patterns('',
     (r'^payu/', include('payu.urls')),
-)
-
-urlpatterns += patterns('example.demo.views',
-    (r'^$', 'home'),
 )
